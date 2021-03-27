11733c627050751df53cdc4e43ad010818eac478
FabricioHang <saveasfabri@gmail.com>
2021-03-27 20:31:57 -0300

    Se realiza dark mode en create-gifos

---
+++ js/create-gifos.js
@@ -0,0 +1,65 @@
+//***************** DARK MODE *******************/
+
+const darkMode = document.querySelector('#dark');
+
+//Para cambiar el colores de fondo, fuentes etc
+darkMode.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+    
+//Para cambiar texto del id="dark" ubicado dentro de .site-nav
+if (darkMode.innerHTML === 'Modo Nocturno') 
+    darkMode.innerHTML = 'Modo Diurno';
+else darkMode.innerHTML = 'Modo Nocturno'; 
+
+	// Guardo en el localstorage.
+if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+} else {
+    localStorage.setItem('dark-mode', 'false');
+}
+});
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    darkMode.innerHTML = 'Modo Diurno';
+}
+else{
+    document.body.classList.remove('dark');
+    darkMode.innerHTML = 'Modo Nocturno';
+}
+
+//************** HOVER ICONS/ SOCIAL MEDIA / .footer *******************/
+
+window.onload = function(){
+    let cambiarIconoHoverFace = document.getElementById("face-icon");
+    let cambiarIconoHovertwit = document.getElementById("twit-icon");
+    let cambiarIconoHoverInsta = document.getElementById("insta-icon");
+
+    cambiarIconoHoverFace.addEventListener('mouseover',cambiarIconoFinalFace);
+    cambiarIconoHoverFace.addEventListener('mouseout',cambiarIconoInicialFace);
+    cambiarIconoHovertwit.addEventListener('mouseover',cambiarIconoFinalTwit);
+    cambiarIconoHovertwit.addEventListener('mouseout',cambiarIconoInicialTwit);
+    cambiarIconoHoverInsta.addEventListener('mouseover',cambiarIconoFinalInsta);
+    cambiarIconoHoverInsta.addEventListener('mouseout',cambiarIconoInicialInsta);
+   
+    function cambiarIconoFinalFace(){
+        this.setAttribute('src','./asset/icon_facebook_hover.svg');
+    }   
+    function cambiarIconoInicialFace(){
+        this.setAttribute('src',"./asset/icon_facebook.svg");
+    }
+    function cambiarIconoFinalTwit(){
+        this.setAttribute('src','./asset/icon-twitter-hover.svg');   
+    }   
+    function cambiarIconoInicialTwit(){
+        this.setAttribute('src',"./asset/icon-twitter.svg");
+    } 
+    function cambiarIconoFinalInsta(){
+        this.setAttribute('src','./asset/icon_instagram-hover.svg');   
+    }   
+    function cambiarIconoInicialInsta(){
+        this.setAttribute('src',"./asset/icon_instagram.svg");
+    } 
+}
+
+
